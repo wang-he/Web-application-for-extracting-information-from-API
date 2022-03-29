2d46088bfcbb3ab90322f6af72d4dc24509116c9
wang-he <53695916+wang-he@users.noreply.github.com>
2022-03-28 20:24:25 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// var form = $('#getForm');
+// form.submit(function (e) {
+//     e.preventDefault();
+//     var id = $("#getForm :input").val();
+//     let headers = new Headers();
+//     headers.append('Access-Control-Allow-Origin', 'http://localhost:8098/api/person/get/1?access_token=');
+//     headers.append('Access-Control-Allow-Credentials', 'true');
+//     $.ajax({
+//         method: form.attr('method'),
+//         url: "http://localhost:8098/api/person/get/1?access_token=",
+//         headers: headers,
+//         type: "dataType",
+//         success: function (data) {
+//             console.log('Submission was successful.');
+//             console.log(data);
+//         },
+//         error: function (data) {
+//             console.log('An error occurred.');
+//             console.log(data);
+//         },
+//     });
+//   });
+
